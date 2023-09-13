bb85f3690145e9f58b4afbc9211b09e070c5bb26
waris <warisaremou2003@gmail.com>
2023-09-13 23:46:46 +0100

    feat: adding link to github profile in main-nav

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -5,8 +5,10 @@ import { siteConfig } from "@/config/site";
 import { CartSheet } from "../cart/cart-sheet";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { MobileNav } from "./mobile-nav";
+import { useRouter } from "next/navigation";
 
 export function SiteHeader() {
+    const router = useRouter();
     return (
         <header className="sticky top-0 z-40 w-full border-b bg-background">
             <div className="container flex items-center h-16">
@@ -15,7 +17,7 @@ export function SiteHeader() {
                 <div className="flex flex-1 justify-end items-center space-x-4">
                     <nav className="flex items-center space-x-4">
                         <CartSheet />
-                        <Avatar>
+                        <Avatar className="cursor-pointer" onClick={() => router.push("https://github.com/Warisaremou")} >
                             <AvatarImage src="https://github.com/Warisaremou.png" alt="@warisaremou" />
                             <AvatarFallback>WA</AvatarFallback>
                         </Avatar>
